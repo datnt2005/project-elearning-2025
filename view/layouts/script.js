10be22f0bb8671db741e5c6301003499fd3d9994
zeisc2 <169135256+zeisc2@users.noreply.github.com>
2025-04-07 22:05:21 +0700

    add chatbot

--- view/layouts/script.js
+++ view/layouts/script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyB1vzKdeNtRivvx9BI0uXQWemrdbNvE7A0"; // https://aistudio.google.com/apikey
+const API_KEY = "cc; // https://aistudio.google.com/apikey
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
